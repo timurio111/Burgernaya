0cca99e226a9db271368bff9b8facbcf678e0b22
timur <73490907+timurio111@users.noreply.github.com>
2023-12-22 21:40:49 +0300

    Update task_6.sql

--- task_6.sql
+++ task_6.sql
@@ -31,3 +31,61 @@ from  orders o left join loyalty_card lc  on lc.owner_id = o.client_id )
 select client_id, order_day_number, date
 from t1
 where date = creation_date;
+
+
+-- query 4: the address of the restaurant where chicken burgers are most frequently ordered + their quantity
+
+select restaurant_address,
+       count(*) as amount
+from order_content
+         left join orders o on order_content.order_id = o.order_id
+         left join restaurant r on o.restaurant_id = r.restaurant_id
+where product_id = 1
+group by restaurant_address
+order by amount desc
+limit 1;
+
+-- query 5: the amount of chili pepper used between the 17th and 24th of the month, in grams
+select SUM(total_sum) as total_sum
+from (select oc.product_id,
+             mi.ingredient_id,
+             sum(grams * quantity) as total_sum,
+             date
+      from ingredients
+               left join public.menu_item mi ON ingredients.ingredient_id = mi.ingredient_id
+               left join order_content oc ON mi.product_id = oc.product_id
+               left join orders o ON oc.order_id = o.order_id
+      where (oc.product_id = 3)
+        and (mi.ingredient_id = 3)
+        and (
+          date between '2023-01-17'
+              and '2023-01-24'
+          )
+      group by oc.product_id,
+               mi.ingredient_id,
+               date) as subquery;
+
+--query 6: displays all drinks that were ordered at the restaurant located at Lermontov 5 + their quantity
+
+select
+  ingredient_name,
+  count(*) as amount
+from
+  restaurant
+  left join orders o on restaurant.restaurant_id = o.restaurant_id
+  left join order_content oc on o.order_id = oc.order_id
+  left join menu_item mi on oc.product_id = mi.product_id
+  left join ingredients i on mi.ingredient_id = i.ingredient_id
+where
+  (o.restaurant_id = 2)
+  and (
+    i.ingredient_id = 9
+    or i.ingredient_id = 10
+  )
+group by
+  ingredient_name
+order by
+  amount desc
+
+
+
