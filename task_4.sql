3ad10cd22731f51141f2eca3e46d66860d3987d7
SkylineUwU <112541432+SkylineUwU@users.noreply.github.com>
2023-12-22 22:19:13 +0300

    Update task_4.sql
    fixed orders(loyalty cards usage) + added new loyalty card

--- task_4.sql
+++ task_4.sql
@@ -56,24 +56,25 @@ INSERT INTO menu_item (product_id, ingredient_id, grams) VALUES (5, 10, 600);
 INSERT INTO menu_item (product_id, ingredient_id, grams) VALUES (6, 9, 300);
 
 INSERT INTO loyalty_card (owner_id, creation_date) VALUES (1, '2023-01-19');
+INSERT INTO loyalty_card (owner_id, creation_date) VALUES (3, '2023-01-18');
 
 INSERT INTO orders (employee_id, client_id, restaurant_id, date, tip, loyalty_card) VALUES (1, 1, 1, '2023-01-15', 100, false);
 INSERT INTO orders (employee_id, client_id, restaurant_id, date, tip, loyalty_card) VALUES (2, 2, 2, '2023-01-16', 150, false);
-INSERT INTO orders (employee_id, client_id, restaurant_id, date, tip, loyalty_card) VALUES (3, 5, 1, '2023-01-17', 0, true);
+INSERT INTO orders (employee_id, client_id, restaurant_id, date, tip, loyalty_card) VALUES (3, 5, 1, '2023-01-17', 0, false);
 INSERT INTO orders (employee_id, client_id, restaurant_id, date, tip, loyalty_card) VALUES (3, 3, 1, '2023-01-17', 50, false);
 INSERT INTO orders (employee_id, client_id, restaurant_id, date, tip, loyalty_card) VALUES (2, 4, 2, '2023-01-17', 50, false);
 INSERT INTO orders (employee_id, client_id, restaurant_id, date, tip, loyalty_card) VALUES (4, 1, 2, '2023-01-17', 100, false);
-INSERT INTO orders (employee_id, client_id, restaurant_id, date, tip, loyalty_card) VALUES (3, 2, 1, '2023-01-17', 0, true);
-INSERT INTO orders (employee_id, client_id, restaurant_id, date, tip, loyalty_card) VALUES (1, 3, 1, '2023-01-18', 0, false);
-INSERT INTO orders (employee_id, client_id, restaurant_id, date, tip, loyalty_card) VALUES (2, 2, 2, '2023-01-19', 0, true);
-INSERT INTO orders (employee_id, client_id, restaurant_id, date, tip, loyalty_card) VALUES (2, 1, 2, '2023-01-19', 200, false);
+INSERT INTO orders (employee_id, client_id, restaurant_id, date, tip, loyalty_card) VALUES (3, 2, 1, '2023-01-17', 0, false);
+INSERT INTO orders (employee_id, client_id, restaurant_id, date, tip, loyalty_card) VALUES (1, 3, 1, '2023-01-18', 0, true);
+INSERT INTO orders (employee_id, client_id, restaurant_id, date, tip, loyalty_card) VALUES (2, 2, 2, '2023-01-19', 0, false);
+INSERT INTO orders (employee_id, client_id, restaurant_id, date, tip, loyalty_card) VALUES (2, 1, 2, '2023-01-19', 200, true);
 INSERT INTO orders (employee_id, client_id, restaurant_id, date, tip, loyalty_card) VALUES (4, 5, 2, '2023-01-20', 0, false);
 INSERT INTO orders (employee_id, client_id, restaurant_id, date, tip, loyalty_card) VALUES (3, 4, 1, '2023-01-21', 50, false);
 INSERT INTO orders (employee_id, client_id, restaurant_id, date, tip, loyalty_card) VALUES (3, 2, 1, '2023-01-21', 0, false);
-INSERT INTO orders (employee_id, client_id, restaurant_id, date, tip, loyalty_card) VALUES (2, 3, 2, '2023-01-23', 150, false);
+INSERT INTO orders (employee_id, client_id, restaurant_id, date, tip, loyalty_card) VALUES (2, 3, 2, '2023-01-23', 150, true);
 INSERT INTO orders (employee_id, client_id, restaurant_id, date, tip, loyalty_card) VALUES (1, 2, 1, '2023-01-24', 100, false);
 INSERT INTO orders (employee_id, client_id, restaurant_id, date, tip, loyalty_card) VALUES (1, 4, 1, '2023-01-24', 0, false);
-INSERT INTO orders (employee_id, client_id, restaurant_id, date, tip, loyalty_card) VALUES (4, 5, 2, '2023-01-24', 500, true);
+INSERT INTO orders (employee_id, client_id, restaurant_id, date, tip, loyalty_card) VALUES (4, 5, 2, '2023-01-24', 500, false);
 
 
 INSERT INTO order_content (order_id, product_id, quantity) VALUES (17, 6, 1);
